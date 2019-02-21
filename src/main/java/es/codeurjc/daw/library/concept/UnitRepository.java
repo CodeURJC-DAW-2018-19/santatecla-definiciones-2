9fc71ac3eef530e73032d9083d64707a77c4e97b
Rod <rod98perez@gmail.com>
2019-02-21 14:00:12 +0100

    Implementación de bases de datos mySQL.
    Implementación de bases de datos mySQL con tablas generadas por conceptos (concepts) y temas (concept units).
    
    También se añaden datos de prueba a la base de datos directamenten desde java.

---
+++ src/main/java/es/codeurjc/daw/library/concept/UnitRepository.java
@@ -0,0 +1,9 @@
+package es.codeurjc.daw.library.concept;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UnitRepository extends JpaRepository<ConceptUnit, Long> {
+
+}
